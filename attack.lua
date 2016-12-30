b095e81f33996be36c25d0a9eee2aaa199b37219
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-12-30 23:10:28 +0100

    initial commit

---
+++ attack.lua
@@ -0,0 +1,26 @@
+local attacking
+
+local f = CreateFrame'Frame'
+f:RegisterEvent'PLAYER_ENTER_COMBAT'
+f:RegisterEvent'PLAYER_LEAVE_COMBAT'
+f:SetScript('OnEvent', function()
+	shooting = event == 'PLAYER_ENTER_COMBAT'
+end)
+
+SLASH_ATTACK1 = '/attack'
+function SlashCmdList.ATTACK(command)
+	if not attacking then
+		CastSpellByName'Attack'
+	end
+end
+
+f:SetScript('OnUpdate', function()
+	f:SetScript('OnUpdate', nil)
+	for i = 1, GetNumMacros() do
+		if GetMacroInfo(i) == 'attack' then
+			EditMacro(i, 'attack', 43, '/attack', 1)
+			return
+		end
+	end
+	CreateMacro('attack', 43, '/attack', 1)
+end)
